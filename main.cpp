b627b74c2f41f15db6b173346917b64fd7f05f28
Vansh Tomar <vansh.tomar.0306@gmail.com>
2025-11-21 12:17:39 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a, b;
+    char op;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter an operator (+, -, *, /): "; 
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    switch(op){
+        case '+':
+            cout << "Result: " << a + b << endl;
+            break;
+        case '-':
+            cout << "Result: " << a - b << endl;
+            break;
+        case '*':
+            cout << "Result: " << a * b << endl;
+            break;
+        case '/':
+            if(b != 0)
+                cout << "Result: " << a / b << endl;
+            else
+                cout << "Error: Division by zero!" << endl;
+            break;
+        default:
+            cout << "Error: Invalid operator!" << endl;
+    }
+
+    return 0;
+}
